fdb1cbf15cdedc424e9ac7d24443cff28ee71587
Salompablo <pablosalompita@gmail.com>
2022-02-02 14:07:30 +0100

    Add products container and API call

--- src/app/component/product/product.component.ts
+++ src/app/component/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+  
+  /** Almacenador de productos */
 
-  constructor() { }
+  public productList : any;
+  constructor(private api : ApiService) { }
 
   ngOnInit(): void {
+    this.api.getProduct()
+    .subscribe(res=>{
+      this.productList = res;
+    })
   }
+
 
 }
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+  
+  /** Función para obtener algun producto de la API */
+
+  getProduct(){
+    return this.http.get<any>("https://my-json-server.typicode.com/franlindebl/shopeame-api-v2/products")
+    .pipe(map((res:any)=>{
+      return res;
+    }))
 }
+}
+
