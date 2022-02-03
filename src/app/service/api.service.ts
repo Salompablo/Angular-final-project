fe98b1b092532171225244a7684dc8cb4685be27
Salompablo <pablosalompita@gmail.com>
2022-02-03 13:48:54 +0100

    Add product container and some style

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -9,7 +9,7 @@ export class ApiService {
 
   constructor(private http : HttpClient) { }
   
-  /** Función para obtener algun producto de la API */
+  /** Función para obtener productos de la API */
 
   getProduct(){
     return this.http.get<any>("https://my-json-server.typicode.com/franlindebl/shopeame-api-v2/products")
